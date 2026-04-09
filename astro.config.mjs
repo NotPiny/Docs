5a3e63ad98d9976997b44d9ad54f3bbe29f017a9
NotPiny <103984634+NotPiny@users.noreply.github.com>
2026-04-10 00:22:46 +0100

    Configure starlight and add PineLib documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,16 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Embers Project Documentation',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/NotPiny' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'PineLib',
+					autogenerate: {
+						directory: 'PineLib',
+					},
+					collapsed: true
+				}
 			],
 		}),
 	],
